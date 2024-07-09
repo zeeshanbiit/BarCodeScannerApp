882d35bfb920092269f7672c7400a46f26aa3a49
zeeshanbiit <zeeshanbiit786@gmail.com>
2024-07-10 02:23:32 +0500

    Initial Commit

---
+++ BarCodeScannerApp/BarCodeScannerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerAppApp.swift
+//  BarCodeScannerApp
+//
+//  Created by Muhammad Zeshan on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
